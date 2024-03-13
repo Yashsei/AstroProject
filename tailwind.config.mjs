1907728cfcd0163a4da61cdf0e00b43f71c1716c
Rodrigo <rodrigoart04@gmail.com>
2024-03-13 16:31:19 -0600

    I create my first component

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors: {
+				primaryRed:	"#b91c1c" ,
+				primaryPurple: "#7c3aed", // Pur
+					},
+				},
+	plugins: [],
+			}
+
+}
